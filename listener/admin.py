478ca1eaa9d812d01ebcdaca8a00524acbaa9df0
Adrián Deccico <adeccico@atlassian.com>
2012-09-23 16:07:35 +1000

    fixing listener model and adding admin panel

---
+++ listener/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from listener.models import *
+
+admin.site.register(Listener)
+admin.site.register(Language)
+admin.site.register(Accent)
--- listener/models.py
+++ listener/models.py
@@ -2,17 +2,29 @@ from django.db import models
 
 
 class Language(models.Model):
+    def __unicode__(self):
+       return self.name 
+
     name = models.CharField(max_length=50)
 
 #object that we can listen (nothing to do with the pattern)
 class Listener(models.Model):
-    src = models.CharField(max_length=1024)
-    pub_date = models.DateTimeField('date published')
-    update_date = models.DateTimeField()
+    def __unicode__(self):
+       return "%s-%s-%s" % (self.language, self.title, self.url)
+
+    url = models.URLField()
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1024, blank=True)
+    pub_date = models.DateTimeField('date published',auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     broken = models.BooleanField()
     language = models.ForeignKey(Language)
+    length = models.SmallIntegerField(blank=True, null=True) #lenght in seconds
 
-class Accents(models.Model):
+class Accent(models.Model):
+    def __unicode__(self):
+       return self.name 
+
     name = models.CharField(max_length=50)
     language = models.ForeignKey(Language)
 
