e9507b94ce4697c2e641cf951f2cced43d1de01d
Adrián Deccico <adeccico@atlassian.com>
2012-12-30 12:10:30 +1100

    getting formatted dialog from the model

--- listener/models.py
+++ listener/models.py
@@ -22,17 +22,18 @@ class Listener(models.Model):
     length = models.SmallIntegerField(blank=True, null=True) #lenght in seconds
     dialog = models.TextField(max_length=8192, blank=True)
     
-    def get_formatted_dialog(self, dialog):
+    @property
+    def formatted_dialog(self):
         """
         Format a dialog from this json format to nice html output
         [{0:'Hutz',1:'Bart'}, 
         [0,"All right, Gentlemen, I' retainer."], 
         [1,"No, money down! Oops, it."], ]        
         """
-        dialog = json.loads(dialog)
+        fdialog = json.loads(self.dialog)
         characters = {}
-        for i in range(len(dialog[0])):
-            characters[i] = dialog[0][i]
+        for i in range(len(fdialog[0])):
+            characters[i] = fdialog[0][i]
 
         html_output = """<table class="table table-striped table-condensed" id="dialog">
             <thead>
@@ -44,7 +45,7 @@ class Listener(models.Model):
             </thead>
             <tbody>
             """
-        for d in dialog[1:]:
+        for d in fdialog[1:]:
             html_output += """
                             <tr><td><strong>%s:</strong></td>
                             <td>%s</td></tr>
@@ -63,6 +64,17 @@ class Listener(models.Model):
                         </table>        
                         """
         return html_output
+    
+    def corrected_dialog(self):
+        #                <tr class="status-correct">
+        #                    <td><strong>Hutz:</strong></td>
+        #                    <td class="status-correct">
+        #                    No, money down! Oops, it shouldn't have this Bar Association 
+        #                    logo here either.
+        #                    </td>
+        #                </tr>
+        pass
+
 
 class Accent(models.Model):
     def __unicode__(self):
