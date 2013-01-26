95abd82c76ecd4a0331de2ef7f123acad531ba7f
Adrián Deccico <adeccico@atlassian.com>
2013-01-26 16:28:02 +1100

    testing that the correct word is returned

--- listener/tests.py
+++ listener/tests.py
@@ -44,3 +44,16 @@ class ListenerTest(TestCase):
         is_correct = cor.correct_dialog(correct_line, maybe_correct_line)
         self.assertTrue(is_correct[0], """error in contraction engine or contraction data
         correct line: '%s' check line: '%s'""" % (correct_line, maybe_correct_line))
+
+    def test_is_preserved(self):
+        correct_line = "All right, Gentlemen, I'll take your case. But I'm going to have to ask for a thousand dollar retainer."
+        user_input = correct_line
+        cor = corrector.Corrector()
+        is_correct,out = cor.correct_dialog(correct_line, user_input)
+        self.assertTrue(is_correct, "Error correcting %s even when the input and correct line are the same." % correct_line)
+        logger.info("user_input: %s corrected: %s" % (user_input, out))
+        for c in out:
+            for u in user_input.split(" "):
+                self.assertEquals(c[1], u, "Correct function does not match with user input in: %s vs %s" % (c[1], u))
+
+        
