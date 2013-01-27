1b6c1f1c0ac2fc1c7ba24bf51088b826bb4bc5b0
Adrián Deccico <adeccico@atlassian.com>
2013-01-27 21:15:33 +1100

    testing that user input is preserved in case of errors

--- listener/tests.py
+++ listener/tests.py
@@ -44,7 +44,7 @@ class ListenerTest(TestCase):
     def test_you_have(self):
         self.try_correction("listen you've to", "listen you Have to")
 
-    def test_you_have(self):
+    def test_you_have_to_listen(self):
         self.try_correction("you've to listen", "you Have to Listen")
 
     def test_have_you(self):
@@ -73,18 +73,23 @@ class ListenerTest(TestCase):
                           """ % (correct_line, out)) 
         for i in range(len(correct_line_list)):
             self.assertEquals(correct_line_list[i], out[1][i][1],
-                              """Correct line has a different length than the output.
+                              """Correct line is different than the output.
                               Correct:%s out:%s. Correct line word:%s output word:%s 
                               """ % (correct_line, out, correct_line_list[i], out[1][i][1])) 
         return out[1]    
     
     def test_mistake(self):
         cor = corrector.Corrector()
-        maybe_correct_line = "aren't great"
+        maybe_correct_line = "aren't Great"
         correct_line = "are not grate"
         logger.debug("maybe correct:'%s' correct line:'%s'" % (maybe_correct_line, correct_line))
         out = cor.correct_dialog(correct_line, maybe_correct_line)
         self.assertFalse(out[0], """error in correction correct line: '%s' check 
         line: '%s' out: %s""" % (correct_line, maybe_correct_line, out))
+        last_word = maybe_correct_line.split(" ")[-1]
+        self.assertEquals(last_word, out[1][-1][1],
+                          """incorrect words should be preserved in the same format.
+                          user input:%s out:%s. Correct line word:%s output word:%s 
+                          """ % (maybe_correct_line, out, last_word, out[1][-1][1])) 
         
         
