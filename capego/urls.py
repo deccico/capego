50bcd9b2ac1e3d326e63c714fbddf958e760ba5a
Adrián Deccico <adeccico@atlassian.com>
2013-02-05 00:59:48 +1100

    adding about and contact pages

--- capego/urls.py
+++ capego/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 
 admin.autodiscover()
@@ -7,5 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^listen/', include('listener.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'', include('listener.urls')),
+    url(r'', include('listener.urls')),
+    url(r'^index\.html/', include('listener.urls')),
+    (r'^contact/$', direct_to_template, {'template': 'contact.html'}),
+    (r'^about/$', direct_to_template, {'template': 'about.html'}),
 )
