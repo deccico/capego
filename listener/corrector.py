d4aae373e93ebec5ab34ab71d615a2e51430e47e
Adrián Deccico <adeccico@atlassian.com>
2013-02-08 00:43:34 +1100

    #10 cleaning inputs

--- listener/corrector.py
+++ listener/corrector.py
@@ -5,6 +5,7 @@ Created on 01/01/2013
 '''
 
 from django.conf import settings
+from django.utils.html import strip_tags
 
 # import the logging library
 import logging
@@ -26,6 +27,8 @@ class Corrector():
         self.contractions = listener.contractions
     
     def correct_dialog(self, good_one, user_input):
+        user_input = strip_tags(user_input)
+
         #todo: eventually we may need to split in "." and "," 
         #without spaces but keeping the signs
         good_one = good_one.strip()
