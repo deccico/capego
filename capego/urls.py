418949bf0638e4532dff478f5841c5a15b30f0e4
Adrián Deccico <adeccico@atlassian.com>
2013-02-25 00:09:45 +1100

    adding supplementary auth files

--- capego/urls.py
+++ capego/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.simple import direct_to_template
 
-from app.views import home, done, logout, error, form, form2, close_login_popup
-from app.facebook import facebook_view
+#from app.views import home, done, logout, error, form, form2, close_login_popup
+#from app.facebook import facebook_view
 
 
 admin.autodiscover()
@@ -17,6 +17,5 @@ urlpatterns = patterns('',
     (r'^contact/$', direct_to_template, {'template': 'contact.html'}),
     (r'^about/$', direct_to_template, {'template': 'about.html'}),
 
-    url(r'^fb/', facebook_view, name='fb_app'),
-    url(r'', include('social_auth.urls')),
+    #url(r'^fb/', facebook_view, name='fb_app'),
 )
