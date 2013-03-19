45527e986e87cb7432e165694f24ea2d562c81b6
Adrián Deccico <adeccico@atlassian.com>
2013-03-20 01:28:37 +1100

    fixing badges url and redirection

--- user_data/views.py
+++ user_data/views.py
@@ -6,15 +6,6 @@ from user_data.models import UsersBadge
 
 @login_required
 def user_badges(request):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect('user')
-
     user_badges = UsersBadge.objects.filter(user=request.user).order_by('badge.id').order_by('-award_date')
-    return render_to_response('/user/badges.html', user_badges)
-    # url(r'^badges$',
-    #     ListView.as_view(
-    #         queryset=UsersBadge.objects.get().order_by('-award_date'),
-    #         context_object_name='user_badges',
-    #         template_name='user/badges.html')),
-    #return render_to_response('user/badges.html', {'version': version},
-    #                          RequestContext(request))
+    return render_to_response('user/badges.html', user_badges)
+
