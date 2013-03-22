f634fc96e35de96f8d15664f4028ec08c93c3901
Adrián Deccico <adeccico@atlassian.com>
2013-03-23 00:27:46 +1100

    badges page now working

--- user_data/views.py
+++ user_data/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from user_data.models import UsersBadge
 
 
 @login_required
 def user_badges(request):
-    user_badges = UsersBadge.objects.filter(user=request.user).order_by('badge','-award_date')
-    return render_to_response('user/badges.html', {'userbadges': user_badges})
+    user_badges = UsersBadge.objects.filter(user=request.user).order_by('-badge','-award_date')
+    return render_to_response('user/badges.html', {'user_badges': user_badges}, RequestContext(request))
 
