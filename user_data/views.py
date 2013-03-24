ba44c890e4391bc5c07a5d9505e80b5824b5d028
Adrián Deccico <adeccico@atlassian.com>
2013-03-25 01:11:01 +1100

    updating badge page and updating font awesome to 3.0

--- user_data/views.py
+++ user_data/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from user_data.models import UsersBadge
+from user_data.models import UsersBadge, Badge
 
 
 @login_required
 def user_badges(request):
-    user_badges = UsersBadge.objects.filter(user=request.user).order_by('-badge','-award_date')
-    return render_to_response('user/badges.html', {'user_badges': user_badges}, RequestContext(request))
+    user_badges = UsersBadge.objects.filter(user=request.user)
+    ub_ids = []
+    for ub in user_badges:
+        ub_ids.append(ub.badge.id)
+    badges = Badge.objects.all().order_by('name')
+    for b in badges:
+        b.awarded = b.id in ub_ids
+    return render_to_response('user/badges.html', {'badges': badges}, RequestContext(request))
 
