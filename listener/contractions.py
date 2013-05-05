180e98bf4bee6e8ea6aaf58abb409892a0278dbc
Adrián Deccico <adeccico@atlassian.com>
2013-05-05 20:50:33 +1000

    adding more videos. 14 in total. Adding also more videos to the front page

--- listener/contractions.py
+++ listener/contractions.py
@@ -1,14 +1,6 @@
-'''
-Created on 12/01/2013
-
-@author: adrian
-'''
-
 from django.conf import settings
 
-# import the logging library
 import logging
-# Get an instance of a logger
 logger = logging.getLogger(settings.APP_NAME)
 
 import os
