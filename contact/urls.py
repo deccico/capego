c117ceefa41b6dc8eaae5fce606d63ae0ef15347
Adrián Deccico <adeccico@atlassian.com>
2013-06-04 02:16:54 +1000

    adding donations

--- contact/urls.py
+++ contact/urls.py
@@ -6,6 +6,7 @@ from contact.views import message
 urlpatterns = patterns('',
                        (r'^$', TemplateView.as_view(template_name="contact/contact.html")),
                        (r'^about/$', TemplateView.as_view(template_name="contact/about.html")),
+                       (r'^donate/$', TemplateView.as_view(template_name="contact/donate.html")),
                        url(r'^message/$', message, name='message'),
                        url(r'^subscribe/$', 'contact.views.subscribe'),
 )
