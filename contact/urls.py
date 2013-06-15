9fa1fa2aa9e1a9b8984bd02dc5f393ebd1b2ccd1
Adrián Deccico <adeccico@atlassian.com>
2013-06-15 13:24:36 +1000

    finishing donation and thanks pages

--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
                        (r'^$', TemplateView.as_view(template_name="contact/contact.html")),
                        (r'^about/$', TemplateView.as_view(template_name="contact/about.html")),
                        (r'^donate/$', TemplateView.as_view(template_name="contact/donate.html")),
+                       (r'^thanks/$', TemplateView.as_view(template_name="contact/thanks.html")),
                        url(r'^message/$', message, name='message'),
                        url(r'^subscribe/$', 'contact.views.subscribe'),
 )
